553760c17e139f1189dac425f21d716bcb9165d8
okue_hidetaka <okue@as-systems.jp>
2023-07-09 10:17:37 +0900

    firstCommit

---
+++ DIPokemon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DIPokemon
+//
+//  Created by 奥江英隆 on 2023/07/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
